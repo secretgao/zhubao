42e62a475bcd4305286b65d4191a437c8997b90a
韭菜 <891841626@qq.com>
2024-06-10 16:51:26 +0800

    *

---
+++ resources/views/product/edit.blade.bak.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>深信国检珠宝检测鉴定中心证书查询</title>
+    <link rel="stylesheet" href="{{asset('images/style.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{asset('images/layui/css/layui.css')}}">
+    <script src="{{asset('images/layui/layui.js')}}"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+
+<body>
+
+<table width="100%" border="0" style="padding-top:30px;">
+    <tr>
+        <td align="center" valign="middle" style="padding:0px; background-color: white;"><img src="{{asset('images/y1.jpg') }}" style="width:100%"></td>
+    </tr>
+    <tr>
+</table>
+
+<table width="100%" border="0" style="padding:20px;">
+    <tr>
+        <td colspan="2" style="border-radius:10px; border:0px solid beige;text-align:center; padding:10px;background-color:cadetblue;color:white;font-size:12pt"><b>深信国检珠宝检测鉴定中心证书查询</b></td>
+    </tr>
+</table>
+
+<div class="cpimg"><img src="{{$imagePath}}" width="300" height="300" /></div>
+
+<div class="sj_bg">
+    <form id="uploadForm">
+    <table width="90%" style="margin:0 auto;">
+        <tr>
+            <td align="left" class="td01">证书编号</td>
+            <td class="td02"></td>
+            <td align="center" class="td01">{{$info->certificate_number}}</td>
+            <input type="hidden" name="certificate_number" value="{{$info->certificate_number}}">
+        </tr>
+        <tr>
+            <td align="left" class="td03">查询编码</td>
+            <td class="td02"></td>
+            <td align="center" class="td03">{{$info->query_code}}</td>
+            <input type="hidden" name="query_code" value="{{$info->query_code}}">
+        </tr>
+        <tr>
+            <td align="left" class="td01">申报名称</td>
+            <td class="td02"></td>
+            <td align="center" class="td01">
+                <input type="text" name="declaration_name" value="{{$info->declaration_name}}">
+            </td>
+        </tr>
+        <tr>
+            <td align="left" class="td03">产品形状</td>
+            <td class="td02"></td>
+            <td align="center" class="td03">
+                <input type="text" name="product_shape" value="{{$info->product_shape}}">
+            </td>
+        </tr>
+        <tr>
+            <td align="left" class="td01">样品质量</td>
+            <td class="td02"></td>
+            <td align="center" class="td01">
+                <input type="text" name="sample_quality" value="{{$info->sample_quality}}">
+            </td>
+        </tr>
+        <tr>
+            <td align="left" class="td03">放大检测</td>
+            <td class="td02"></td>
+            <td align="center" class="td03">
+                <input type="text" name="amplification" value="{{$info->amplification}}">
+            </td>
+        </tr>
+        <tr>
+            <td align="left" class="td01">检测结果</td>
+            <td class="td02"></td>
+            <td align="center" class="td01">
+                <input type="text" name="detection" value="{{$info->detection}}">
+            </td>
+        </tr>
+        <tr>
+            <td align="left" class="td03">拉丁名称</td>
+            <td class="td02"></td>
+            <td align="center" class="td03">
+                <input type="text" name="detection_1" value="{{$info->detection_1}}">
+            </td>
+        </tr>
+        <tr>
+            <td align="left" class="td01">执行标准</td>
+            <td class="td02"></td>
+            <td align="center" class="td01">GB/T16552 GB/T16553 GB.T18043</td>
+        </tr>
+        <tr>
+            <td align="left" class="td03">主检</td>
+            <td class="td02"></td>
+            <td align="center" class="td03">李洁</td>
+        </tr>
+        <tr>
+            <td align="left" class="td01">审核</td>
+            <td class="td02"></td>
+            <td align="center" class="td01">王福珍</td>
+        </tr>
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    </table>
+
+        <div class="dyzm">
+            <div class="dyzm1"><button type="submit">保存</button></div>
+        </div>
+    </form>
+</div>
+
+<table width="100%" border="0" style="padding:20px;">
+    <tr>
+        <td colspan="2" style="border-radius:10px; border:0px solid beige;text-align:center; padding:10px;background:#db0000;color:white;font-size:12pt"><b>本检验证书只对本产品负责，翻印无效。</b></td>
+    </tr>
+</table>
+
+
+
+<div style=" height:500px;"></div>
+
+</body>
+
+<script>
+
+    $('#uploadForm').submit(function(e) {
+        e.preventDefault(); // 阻止表单默认提交行为
+
+        var formData = $(this).serialize(); // 序列化表单数据
+
+        $.ajax({
+            type: 'POST',
+            url:  "{{route('product.update')}}", // 提交到的 URL
+            data: formData,
+            success: function (response) {
+                // 成功提交后的回调函数
+                console.log(response);
+                if (response.status == 200){
+                    layer.msg('编辑成功', {icon:100,time:2000});
+                    window.location.href = "{{route('product.list')}}";
+                } else {
+                    layer.msg('编辑失敗：'+response.msg, {icon:100,time:2000});
+                }
+            },
+            error: function () {
+                // 提交出错的回调函数
+                alert('表单提交失败!');
+            }
+        });
+    })
+</script>
+</html>
--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -2,125 +2,87 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>深信国检珠宝检测鉴定中心证书查询</title>
+    <title>深信国检珠宝检测鉴定中心</title>
     <link rel="stylesheet" href="{{asset('images/style.css') }}" type="text/css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="{{asset('images/layui/css/layui.css')}}">
     <script src="{{asset('images/layui/layui.js')}}"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
+<form id="uploadForm">
+<div class="zsmbadmin">
 
-<table width="100%" border="0" style="padding-top:30px;">
-    <tr>
-        <td align="center" valign="middle" style="padding:0px; background-color: white;"><img src="{{asset('images/y1.jpg') }}" style="width:100%"></td>
-    </tr>
-    <tr>
-</table>
+    <div class="zsmbadmin-box">
 
-<table width="100%" border="0" style="padding:20px;">
-    <tr>
-        <td colspan="2" style="border-radius:10px; border:0px solid beige;text-align:center; padding:10px;background-color:cadetblue;color:white;font-size:12pt"><b>深信国检珠宝检测鉴定中心证书查询</b></td>
-    </tr>
-</table>
+        <div class="t1"><input name="certificate_number" type="text" value="{{ $certificateNumber}}" readonly /></div>
+        <div class="t2"><input name="query_code" type="text" value="{{ $queryCode }}" readonly /></div>
+        <div class="t3"><input name="declaration_name" type="text" value="沉香" /></div>
+        <div class="t4"><input name="product_shape" type="text" value="如图" /></div>
+        <div class="t5"><input name="sample_quality" type="text" value="16mm,15.25g" /></div>
+        <div class="t6"><input name="amplification" type="text" value="带状薄壁组织" /></div>
+        <div class="t7"><input name="detection" type="text" value="瑞香科沉香属" /></div>
+        <div class="t8"><input name="detection_1" type="text" value="Aquilaria sp." /></div>
+        <div class="t9">
 
-<div class="cpimg"><img src="{{$imagePath}}" width="300" height="300" /></div>
+            <input type="file" id="imageUpload" name="image" accept="image/*" onchange="previewImage(event)">
+            <br>
+            <img id="imagePreview" class="preview" src="{{asset('images/up1.jpg') }}" alt="图片预览">
 
-<div class="sj_bg">
-    <form id="uploadForm">
-    <table width="90%" style="margin:0 auto;">
-        <tr>
-            <td align="left" class="td01">证书编号</td>
-            <td class="td02"></td>
-            <td align="center" class="td01">{{$info->certificate_number}}</td>
-            <input type="hidden" name="certificate_number" value="{{$info->certificate_number}}">
-        </tr>
-        <tr>
-            <td align="left" class="td03">查询编码</td>
-            <td class="td02"></td>
-            <td align="center" class="td03">{{$info->query_code}}</td>
-            <input type="hidden" name="query_code" value="{{$info->query_code}}">
-        </tr>
-        <tr>
-            <td align="left" class="td01">申报名称</td>
-            <td class="td02"></td>
-            <td align="center" class="td01">
-                <input type="text" name="declaration_name" value="{{$info->declaration_name}}">
-            </td>
-        </tr>
-        <tr>
-            <td align="left" class="td03">产品形状</td>
-            <td class="td02"></td>
-            <td align="center" class="td03">
-                <input type="text" name="product_shape" value="{{$info->product_shape}}">
-            </td>
-        </tr>
-        <tr>
-            <td align="left" class="td01">样品质量</td>
-            <td class="td02"></td>
-            <td align="center" class="td01">
-                <input type="text" name="sample_quality" value="{{$info->sample_quality}}">
-            </td>
-        </tr>
-        <tr>
-            <td align="left" class="td03">放大检测</td>
-            <td class="td02"></td>
-            <td align="center" class="td03">
-                <input type="text" name="amplification" value="{{$info->amplification}}">
-            </td>
-        </tr>
-        <tr>
-            <td align="left" class="td01">检测结果</td>
-            <td class="td02"></td>
-            <td align="center" class="td01">
-                <input type="text" name="detection" value="{{$info->detection}}">
-            </td>
-        </tr>
-        <tr>
-            <td align="left" class="td03">拉丁名称</td>
-            <td class="td02"></td>
-            <td align="center" class="td03">
-                <input type="text" name="detection_1" value="{{$info->detection_1}}">
-            </td>
-        </tr>
-        <tr>
-            <td align="left" class="td01">执行标准</td>
-            <td class="td02"></td>
-            <td align="center" class="td01">GB/T16552 GB/T16553 GB.T18043</td>
-        </tr>
-        <tr>
-            <td align="left" class="td03">主检</td>
-            <td class="td02"></td>
-            <td align="center" class="td03">李洁</td>
-        </tr>
-        <tr>
-            <td align="left" class="td01">审核</td>
-            <td class="td02"></td>
-            <td align="center" class="td01">王福珍</td>
-        </tr>
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-    </table>
-
-        <div class="dyzm">
-            <div class="dyzm1"><button type="submit">保存</button></div>
+           </div>
+        <div class="t10">
+            <img src="{{$qcurl}}">
+            <input type="hidden" id="qc" name="qc_content" value="{{$qcurl}}">
         </div>
-    </form>
+
+        <input type="hidden" id="imagePath" name="image_path">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    </div>
+
 </div>
-
-<table width="100%" border="0" style="padding:20px;">
-    <tr>
-        <td colspan="2" style="border-radius:10px; border:0px solid beige;text-align:center; padding:10px;background:#db0000;color:white;font-size:12pt"><b>本检验证书只对本产品负责，翻印无效。</b></td>
-    </tr>
-</table>
-
-
-
-<div style=" height:500px;"></div>
+<div class="dyzm">
+    <div class="dyzm1"><button type="submit">保存上传</button></div>
+    <div class="dyzm1 dyzm2"><a href="zsdy.html" target="_blank">打印证书</a></div>
+    <div class="dyzm1 dyzm2"><a href="zsdy-bm.html" target="_blank">打印背面</a></div>
+</div>
+</form>
 
 </body>
-
+</html>
 <script>
 
+    function previewImage(event) {
+        const input = event.target;
+        const reader = new FileReader();
+        reader.onload = function(){
+            const dataURL = reader.result;
+            const output = document.getElementById('imagePreview');
+            output.src = dataURL;
+        };
+        reader.readAsDataURL(input.files[0]);
+
+        var formData = new FormData();
+        var imageFile = $('#imageUpload')[0].files[0];
+        var token = $('#uploadForm input[name="_token"]').val();
+
+        formData.append('image', imageFile);
+        formData.append('_token', token);
+
+        $.ajax({
+            url: '/upload/file', // 替换为你的服务器端点
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+                console.log('Success:', response);
+                $('#imagePath').val(response.path)
+            },
+            error: function(xhr, status, error) {
+                console.log('Error:', error);
+            }
+        });
+    }
     $('#uploadForm').submit(function(e) {
         e.preventDefault(); // 阻止表单默认提交行为
 
@@ -128,16 +90,16 @@
 
         $.ajax({
             type: 'POST',
-            url:  "{{route('product.update')}}", // 提交到的 URL
+            url: "{{route('upload.upload')}}", //URL
             data: formData,
             success: function (response) {
                 // 成功提交后的回调函数
                 console.log(response);
                 if (response.status == 200){
-                    layer.msg('编辑成功', {icon:100,time:2000});
+                    layer.msg('提交成功!', {icon:100,time:2000});
                     window.location.href = "{{route('product.list')}}";
                 } else {
-                    layer.msg('编辑失敗：'+response.msg, {icon:100,time:2000});
+                    layer.msg('提交失敗：'+response.msg, {icon:100,time:2000});
                 }
             },
             error: function () {
@@ -147,4 +109,4 @@
         });
     })
 </script>
-</html>
+
